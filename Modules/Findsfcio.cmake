affa210e1d4cbcc8577ced938c19cf935771c553
Xianwu.Xue <Xianwu.Xue@noaa.gov>
2021-10-16 00:26:22 -0400

    Update Find[sfcio,sigio] using updated sfcio,sigio  On branch feature/port2acorn_gefsv12  Changes to be committed: 	modified:   Modules/Findsfcio.cmake 	modified:   Modules/Findsigio.cmake
    Refs: #53

--- Modules/Findsfcio.cmake
+++ Modules/Findsfcio.cmake
@@ -1,63 +1,30 @@
 # This modulerlooks for environment variables detailing where SFCIO lib is
 # If variables are not set, SFCIO will be built from external source
-if(DEFINED ENV{SFCIO_LIB4} )
-  set(SFCIO_LIB4 $ENV{SFCIO_LIB4} CACHE STRING "SFCIO_4 Library Location" )
-  set(SFCIO_INC4 $ENV{SFCIO_INC4} CACHE STRING "SFCIO_4 Include Location" )
-  
-  set(name "sfcio")
-  string(TOUPPER ${name} uppercase_name)
-
-  string(REGEX MATCH "(v[0-9]+\\.[0-9]+\\.[0-9]+)" _ ${${uppercase_name}_LIB4})
-  set(version ${CMAKE_MATCH_1})
-
-  set(kinds "4")
-  foreach(kind ${kinds})
-    set(lib_name ${name}_${kind})
-    set(versioned_lib_name ${name}_${version}_${kind})
-
-    if(EXISTS ${${uppercase_name}_LIB${kind}} )
-      get_filename_component(lib_dir ${${uppercase_name}_LIB${kind}} DIRECTORY)
-      find_library(sfcio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      if(NOT sfcio_path)
-        set(versioned_lib_name ${name}_${kind})
-        find_library(sfcio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      endif()
-      add_library(${lib_name} STATIC IMPORTED)
-      set_target_properties(${lib_name} PROPERTIES
-        IMPORTED_LOCATION ${sfcio_path}
-        INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
-    endif()
-  endforeach()
-elseif(DEFINED ENV{SFCIO_LIB} )
-  set(SFCIO_LIB4 $ENV{SFCIO_LIB} CACHE STRING "SFCIO_4 Library Location" )
-  set(SFCIO_INC4 $ENV{SFCIO_INC} CACHE STRING "SFCIO_4 Include Location" )
+if(DEFINED ENV{SFCIO_LIB} )
+  set(SFCIO_LIB $ENV{SFCIO_LIB} CACHE STRING "SFCIO Library Location" )
+  set(SFCIO_INC $ENV{SFCIO_INC} CACHE STRING "SFCIO Include Location" )
 
   set(name "sfcio")
   string(TOUPPER ${name} uppercase_name)
 
-  string(REGEX MATCH "(v[0-9]+\\.[0-9]+\\.[0-9]+)" _ ${${uppercase_name}_LIB4})
-  set(version ${CMAKE_MATCH_1})
+  #string(REGEX MATCH "(v[0-9]+\\.[0-9]+\\.[0-9]+)" _ ${${uppercase_name}_LIB})
+  #set(version ${CMAKE_MATCH_1})
 
-  set(kinds "4")
-  foreach(kind ${kinds})
-    set(lib_name ${name}_${kind})
-    set(versioned_lib_name ${name}_${version}_${kind})
-
-    if(EXISTS ${${uppercase_name}_LIB${kind}} )
-      get_filename_component(lib_dir ${${uppercase_name}_LIB${kind}} DIRECTORY)
-      find_library(sfcio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      if(NOT sfcio_path)
-        set(versioned_lib_name ${name}) #_${version}_${kind})
-        find_library(sfcio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      endif()
-      add_library(${lib_name} STATIC IMPORTED)
-      set_target_properties(${lib_name} PROPERTIES
-        IMPORTED_LOCATION ${sfcio_path}
-        INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
+  set(lib_name ${name})
+  #set(versioned_lib_name ${name})
+  if(EXISTS ${${uppercase_name}_LIB} )
+    get_filename_component(lib_dir ${${uppercase_name}_LIB} DIRECTORY)
+    find_library(sfcio_path NAMES ${lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
+    if(NOT sfcio_path)
+      set(lib_name ${name}) #_${version}_${kind})
+      find_library(sfcio_path NAMES ${lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
     endif()
+    add_library(${lib_name} STATIC IMPORTED)
+    set_target_properties(${lib_name} PROPERTIES
+      IMPORTED_LOCATION ${sfcio_path}
+      INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC})
+  endif()
       
-  endforeach()
-    
 endif()
 
 include(FindPackageHandleStandardArgs)
--- Modules/Findsigio.cmake
+++ Modules/Findsigio.cmake
@@ -1,61 +1,27 @@
 # This module looks for environment variables detailing where SIGIO lib is
 # If variables are not set, SIGIO will be built from external source
-if(DEFINED ENV{SIGIO_LIB4} )
-  set(SIGIO_LIB4 $ENV{SIGIO_LIB4} CACHE STRING "SIGIO_4 Library Location" )
-  set(SIGIO_INC4 $ENV{SIGIO_INC4} CACHE STRING "SIGIO_4 Include Location" )
+if(DEFINED ENV{SIGIO_LIB} )
+  set(SIGIO_LIB $ENV{SIGIO_LIB} CACHE STRING "SIGIO Library Location" )
+  set(SIGIO_INC $ENV{SIGIO_INC} CACHE STRING "SIGIO Include Location" )
 
   set(name "sigio")
   string(TOUPPER ${name} uppercase_name)
 
-  string(REGEX MATCH "(v[0-9]+\\.[0-9]+\\.[0-9]+)" _ ${${uppercase_name}_LIB4})
-  set(version ${CMAKE_MATCH_1})
 
-  set(kinds "4")
-  foreach(kind ${kinds})
-    set(lib_name ${name}_${kind})
-    set(versioned_lib_name ${name}_${version}_${kind})
+  set(lib_name ${name})
 
-    if(EXISTS ${${uppercase_name}_LIB${kind}} )
-      get_filename_component(lib_dir ${${uppercase_name}_LIB${kind}} DIRECTORY)
-      find_library(sigio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      if(NOT sigio_path)
-        set(versioned_lib_name ${name}_${kind})
-        find_library(sigio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      endif()
-      add_library(${lib_name} STATIC IMPORTED)
-      set_target_properties(${lib_name} PROPERTIES
-        IMPORTED_LOCATION ${sigio_path}
-        INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
+  if(EXISTS ${${uppercase_name}_LIB} )
+    get_filename_component(lib_dir ${${uppercase_name}_LIB} DIRECTORY)
+    find_library(sigio_path NAMES ${lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
+    if(NOT sigio_path)
+      set(lib_name ${name})
+      find_library(sigio_path NAMES ${lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
     endif()
-  endforeach()
-elseif(DEFINED ENV{SIGIO_LIB} )
-  set(SIGIO_LIB4 $ENV{SIGIO_LIB} CACHE STRING "SIGIO_4 Library Location" )
-  set(SIGIO_INC4 $ENV{SIGIO_INC} CACHE STRING "SIGIO_4 Include Location" )
-
-  set(name "sigio")
-  string(TOUPPER ${name} uppercase_name)
-
-  string(REGEX MATCH "(v[0-9]+\\.[0-9]+\\.[0-9]+)" _ ${${uppercase_name}_LIB4})
-  set(version ${CMAKE_MATCH_1})
-
-  set(kinds "4")
-  foreach(kind ${kinds})
-    set(lib_name ${name}_${kind})
-    set(versioned_lib_name ${name}_${version}_${kind})
-
-    if(EXISTS ${${uppercase_name}_LIB${kind}} )
-      get_filename_component(lib_dir ${${uppercase_name}_LIB${kind}} DIRECTORY)
-      find_library(sigio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      if(NOT sigio_path)
-        set(versioned_lib_name ${name})
-        find_library(sigio_path NAMES ${versioned_lib_name} PATHS ${lib_dir} NO_DEFAULT_PATH)
-      endif()
-      add_library(${lib_name} STATIC IMPORTED)
-      set_target_properties(${lib_name} PROPERTIES
-        IMPORTED_LOCATION ${sigio_path}
-        INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
-    endif()
-  endforeach()
+    add_library(${lib_name} STATIC IMPORTED)
+    set_target_properties(${lib_name} PROPERTIES
+      IMPORTED_LOCATION ${sigio_path}
+      INTERFACE_INCLUDE_DIRECTORIES ${${uppercase_name}_INC${kind}})
+  endif()
 endif()
 
 include(FindPackageHandleStandardArgs)
